13ea28a877d44d2022c634492e3d0b45554ff239
MegaMech <MegaMech@users.noreply.github.com>
2025-04-11 14:47:41 -0600

    attempt 2

--- fast64_internal/mk64/mk64_properties.py
+++ fast64_internal/mk64/mk64_properties.py
@@ -63,6 +63,10 @@ class MK64_ExportProperties(PropertyGroup):
 #    Scene Properties
 # ------------------------------------------------------------------------
 
+featureSetEnum = (
+    ("Decomp", "Decomp", "Decomp"),
+    ("HM64", "HM64", "Harbour Masters"),
+)
 
 class MK64_Properties(PropertyGroup):
     """Global MK64 Scene Properties found under scene.fast64.mk64"""
