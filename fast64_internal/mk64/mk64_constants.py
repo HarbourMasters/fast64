4f33469b52f749d6c532bfb58e6471956a3402c8
MegaMech <MegaMech@users.noreply.github.com>
2025-04-11 13:32:51 -0600

    Create mk64_constants.py

---
+++ fast64_internal/mk64/mk64_constants.py
@@ -0,0 +1,54 @@
+MODEL_HEADER = """#include <ultra64.h>
+#include <macros.h>
+#include <PR/gbi.h>
+
+"""
+
+enum_surface_types = [
+    ("SURFACE_DEFAULT", "SURFACE_DEFAULT", "SURFACE_DEFAULT"),
+    ("AIRBORNE", "AIRBORNE", "AIRBORNE"),
+    ("ASPHALT", "ASPHALT", "ASPHALT"),
+    ("DIRT", "DIRT", "DIRT"),
+    ("SAND", "SAND", "SAND"),
+    ("STONE", "STONE", "STONE"),
+    ("SNOW", "SNOW", "SNOW"),
+    ("BRIDGE", "BRIDGE", "BRIDGE"),
+    ("SAND_OFFROAD", "SAND_OFFROAD", "SAND_OFFROAD"),
+    ("GRASS", "GRASS", "GRASS"),
+    ("ICE", "ICE", "ICE"),
+    ("WET_SAND", "WET_SAND", "WET_SAND"),
+    ("SNOW_OFFROAD", "SNOW_OFFROAD", "SNOW_OFFROAD"),
+    ("CLIFF", "CLIFF", "CLIFF"),
+    ("DIRT_OFFROAD", "DIRT_OFFROAD", "DIRT_OFFROAD"),
+    ("TRAIN_TRACK", "TRAIN_TRACK", "TRAIN_TRACK"),
+    ("CAVE", "CAVE", "CAVE"),
+    ("ROPE_BRIDGE", "ROPE_BRIDGE", "ROPE_BRIDGE"),
+    ("WOOD_BRIDGE", "WOOD_BRIDGE", "WOOD_BRIDGE"),
+    ("BOOST_RAMP_WOOD", "BOOST_RAMP_WOOD", "BOOST_RAMP_WOOD"),
+    ("OUT_OF_BOUNDS", "OUT_OF_BOUNDS", "OUT_OF_BOUNDS"),
+    ("BOOST_RAMP_ASPHALT", "BOOST_RAMP_ASPHALT", "BOOST_RAMP_ASPHALT"),
+    ("RAMP", "RAMP", "RAMP"),
+    ("WATER", "WATER", "WATER"),
+]
+
+enum_actor_types = [
+    ("Piranha_Plant", "Piranha Plant", "Piranha Plant"),
+    ("Other", "Other", "Other"),
+]
+
+mk64_world_defaults = {
+    "geometryMode": {
+        "zBuffer": True,
+        "shade": True,
+        "cullBack": True,
+        "lighting": True,
+        "shadeSmooth": True,
+    },
+    "otherModeH": {
+        "alphaDither": "G_AD_NOISE",
+        "textureFilter": "G_TF_BILERP",
+        "perspectiveCorrection": "G_TP_PERSP",
+        "textureConvert": "G_TC_FILT",
+        "cycleType": "G_CYC_2CYCLE",
+    },
+}
